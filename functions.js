13f4547de2aba0601aef212592f683d5cd08111f
David O'Regan <oreagn10@gmail.com>
2023-05-24 21:02:11 -0400

    Mobile optimisation pt1

---
+++ functions.js
@@ -0,0 +1,18 @@
+var expanded = false;
+
+function collapsetoggle() {
+    let toToggle = document.getElementById('collapsablemenu');
+    let toToggle2 = document.getElementById('collapsablemenu2');
+    let toToggle3 = document.getElementById('screencover');
+    if (expanded) {
+        toToggle.style.display = 'none';
+        toToggle2.style.display = 'none';
+        toToggle3.style.display = 'none';
+        expanded = false;
+    } else {
+        toToggle.style.display = 'block';
+        toToggle2.style.display = 'flex';
+        toToggle3.style.display = 'block';
+        expanded = true;
+    }
+}
